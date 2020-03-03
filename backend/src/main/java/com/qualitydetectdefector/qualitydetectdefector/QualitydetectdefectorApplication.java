e7db34010f3e862271dcab409915d9ccf45bc825
irem-ustunboyacioglu <iremustun96@hotmail.com>
2020-03-03 17:22:18 +0300

    Initialized backend project.

---
+++ backend/src/main/java/com/qualitydetectdefector/qualitydetectdefector/QualitydetectdefectorApplication.java
@@ -0,0 +1,13 @@
+package com.qualitydetectdefector.qualitydetectdefector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualitydetectdefectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QualitydetectdefectorApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/qualitydetectdefector/qualitydetectdefector/QualitydetectdefectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualitydetectdefector.qualitydetectdefector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualitydetectdefectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
