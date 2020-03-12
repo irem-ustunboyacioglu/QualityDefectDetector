6280f773786e35dad0d5f68f93df93bdc461ca6e
irem-ustunboyacioglu <iremustun96@hotmail.com>
2020-03-12 19:25:35 +0300

    Added swagger.

--- backend/src/main/java/com/qualitydefectdetector/QualityDefectDetectorApplication.java
+++ backend/src/main/java/com/qualitydefectdetector/QualityDefectDetectorApplication.java
@@ -1,9 +1,11 @@
-package com.qualitydefectdetector.api;
+package com.qualitydefectdetector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class QualityDefectDetectorApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/com/qualitydefectdetector/parser/UserStoryParser.java
@@ -0,0 +1,9 @@
+package com.qualitydefectdetector.parser;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserStoryParser {
+
+
+}
