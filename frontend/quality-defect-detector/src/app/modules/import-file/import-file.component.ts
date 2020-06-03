23a3afe53996fdb810109558c64fa1a653e6b610
irem-ustunboyacioglu <iremustun96@hotmail.com>
2020-06-03 23:24:53 +0300

    Changed file download responses

--- frontend/quality-defect-detector/src/app/modules/import-file/import-file.component.ts
+++ frontend/quality-defect-detector/src/app/modules/import-file/import-file.component.ts
@@ -24,6 +24,7 @@ export class ImportFileComponent implements OnInit {
       this.importedFile = event.target.files[0];
     }
   }
+
   submit() {
     if (!this.importedFile) {
       return;
@@ -40,47 +41,69 @@ export class ImportFileComponent implements OnInit {
 
   downloadFile(response: SetOfUserStoriesReportResponse) {
     if (this.importedFile.name.endsWith('.csv') || this.importedFile.name.endsWith('.txt')) {
-      let csvString: string = 'Kriter,Tek/Set,Kullanıcı Hikayesi,Statüs,Hata Mesajı\r\n';
       const { setCriteriaResults, singleUserStoryReportList } = response;
+      let csvString = 'Set Kriterleri,Durum,Hata Mesajı\r\n';
 
       Object.keys(setCriteriaResults).forEach(key => {
-        csvString += `${key},Set,Yok,${setCriteriaResults[key].satisfiesThisCriteria ? 'Geçti' : 'Geçemedi'}, ${setCriteriaResults[key].errorMessage ? setCriteriaResults[key].errorMessage : 'Yok'}\r\n`;
+        csvString += `${key}` + ',' +
+          `${setCriteriaResults[key].satisfiesThisCriteria ? 'Geçti' : 'Geçemedi'}` + ',' +
+          `${setCriteriaResults[key].errorMessage ? setCriteriaResults[key].errorMessage : 'Yok'}\r\n`;
       });
+
+      csvString += 'Kullanıcı Hikayesi,Tam Cümle, Atomik,İyi Biçimlendirilmiş,Minimal, Probleme Yönelik, Yazım\r\n';
 
       singleUserStoryReportList.forEach(userStoryRow => {
-        const { userStory, criteriaCheckResults } = userStoryRow;
-        Object.keys(criteriaCheckResults).forEach(key => {
-          csvString += `${key},Tek,${userStory.userStorySentence},${criteriaCheckResults[key].satisfiesThisCriteria ? 'Geçti' : 'Geçemedi'}, ${criteriaCheckResults[key].errorMessage ? criteriaCheckResults[key].errorMessage : 'Yok'}\r\n`;
-        });
+        const {userStory, criteriaCheckResults} = userStoryRow;
+        csvString += `${userStory.userStorySentence}` + ',' +
+       `${criteriaCheckResults['Tam Cümle'].satisfiesThisCriteria ? 'Geçti' : criteriaCheckResults['Tam Cümle'].errorMessage ? criteriaCheckResults['Tam Cümle'].errorMessage : 'Yok'}` + ',' +
+          `${criteriaCheckResults['Atomik'].satisfiesThisCriteria ? 'Geçti' : criteriaCheckResults['Atomik'].errorMessage ? criteriaCheckResults['Atomik'].errorMessage : 'Yok'}` + ',' +
+          `${criteriaCheckResults['İyi Biçimlendirilmiş'].satisfiesThisCriteria ? 'Geçti' : criteriaCheckResults['İyi Biçimlendirilmiş'].errorMessage ? criteriaCheckResults['İyi Biçimlendirilmiş'].errorMessage : 'Yok'}` + ',' +
+          `${criteriaCheckResults['Minimal'].satisfiesThisCriteria ? 'Geçti' : criteriaCheckResults['Minimal'].errorMessage ? criteriaCheckResults['Minimal'].errorMessage : 'Yok'}` + ',' +
+          `${criteriaCheckResults['Probleme Yönelik'].satisfiesThisCriteria ? 'Geçti' : criteriaCheckResults['Probleme Yönelik'].errorMessage ? criteriaCheckResults['Probleme Yönelik'].errorMessage : 'Yok'}` + ',' +
+          `${criteriaCheckResults['Yazım'].satisfiesThisCriteria ? 'Geçti' : criteriaCheckResults['Yazım'].errorMessage ? criteriaCheckResults['Yazım'].errorMessage : 'Yok'}\r\n`;
       });
-      var blob = new Blob([csvString], { type: `text/${this.importedFile.name.substring(this.importedFile.name.indexOf('.') + 1)}` })
+
+      const blob = new Blob([csvString], {type: `text/${this.importedFile.name.substring(this.importedFile.name.indexOf('.') + 1)}`});
       saveAs(blob, `kullanici-hikayeleri-rapor.${this.importedFile.name.substring(this.importedFile.name.indexOf('.') + 1)}`);
       this.importedFile = undefined;
-    }
-    else if (this.importedFile.name.endsWith('.xlsx')) {
+    } else if (this.importedFile.name.endsWith('.xlsx')) {
       const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-      const { setCriteriaResults, singleUserStoryReportList } = response;
+      const {setCriteriaResults, singleUserStoryReportList} = response;
       const xlsxArray = [];
+      const xlsxArray2 = [];
       Object.keys(setCriteriaResults).forEach(key => {
         xlsxArray.push({
-          'Kriter': key, 'Tek/Set': 'Set', 'Kullanıcı Hikayesi': 'Yok',
-          'Statüs': setCriteriaResults[key].satisfiesThisCriteria ? 'Geçti' : 'Geçemedi',
+          'Set Kriterleri': key, 'Durum': setCriteriaResults[key].satisfiesThisCriteria ? 'Geçti' : 'Geçmedi',
           'Hata Mesajı': setCriteriaResults[key].errorMessage ? setCriteriaResults[key].errorMessage : 'Yok'
         });
       });
       singleUserStoryReportList.forEach(userStoryRow => {
-        const { userStory, criteriaCheckResults } = userStoryRow;
-        Object.keys(criteriaCheckResults).forEach(key => {
-          xlsxArray.push({
-            'Kriter': key, 'Tek/Set': 'Tek', 'Kullanıcı Hikayesi': userStory.userStorySentence,
-            'Statüs': criteriaCheckResults[key].satisfiesThisCriteria ? 'Geçti' : 'Geçemedi',
-            'Hata Mesajı': criteriaCheckResults[key].errorMessage ? criteriaCheckResults[key].errorMessage : 'Yok'
-          });
+        const {userStory, criteriaCheckResults} = userStoryRow;
+        xlsxArray2.push({
+          'Kullanıcı Hikayesi': userStory.userStorySentence,
+          'Tam Cümle': criteriaCheckResults['Tam Cümle'].satisfiesThisCriteria ? 'Geçti' :
+            criteriaCheckResults['Tam Cümle'].errorMessage ? criteriaCheckResults['Tam Cümle'].errorMessage : 'Yok',
+          'Atomik': criteriaCheckResults['Atomik'].satisfiesThisCriteria ? 'Geçti' :
+            criteriaCheckResults['Atomik'].errorMessage ? criteriaCheckResults['Atomik'].errorMessage : 'Yok',
+          'İyi Biçimlendirilmiş': criteriaCheckResults['İyi Biçimlendirilmiş'].satisfiesThisCriteria ? 'Geçti' :
+            criteriaCheckResults['İyi Biçimlendirilmiş'].errorMessage ? criteriaCheckResults['İyi Biçimlendirilmiş'].errorMessage : 'Yok',
+          'Minimal': criteriaCheckResults['Minimal'].satisfiesThisCriteria ? 'Geçti' :
+            criteriaCheckResults['Minimal'].errorMessage ? criteriaCheckResults['Minimal'].errorMessage : 'Yok',
+          'Probleme Yönelik': criteriaCheckResults['Probleme Yönelik'].satisfiesThisCriteria ? 'Geçti' :
+            criteriaCheckResults['Probleme Yönelik'].errorMessage ? criteriaCheckResults['Probleme Yönelik'].errorMessage : 'Yok',
+          'Yazım': criteriaCheckResults['Yazım'].satisfiesThisCriteria ? 'Geçti' :
+            criteriaCheckResults['Yazım'].errorMessage ? criteriaCheckResults['Yazım'].errorMessage : 'Yok',
         });
       });
       const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(xlsxArray);
-      const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-      const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+      const worksheet2: XLSX.WorkSheet = XLSX.utils.json_to_sheet(xlsxArray2);
+      const workbook: XLSX.WorkBook = {
+        Sheets: {
+          'Set Kriter Sonuçları': worksheet,
+          'Kullanıcı Hikaye Sonuçları': worksheet2
+        }, SheetNames: ['Set Kriter Sonuçları', 'Kullanıcı Hikaye Sonuçları']
+      };
+      const excelBuffer: any = XLSX.write(workbook, {bookType: 'xlsx', type: 'array'});
 
       const data: Blob = new Blob([excelBuffer], { type: EXCEL_TYPE });
       saveAs(data, 'kullanici-hikayeleri-rapor.xlsx');
